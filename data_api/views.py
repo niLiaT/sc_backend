bd24432d9db66c2c3fa34aa277fc1dff912e8335
chiutailin <curlyajiu@gmail.com>
2021-06-23 22:17:58 +0800

    feat: Add APIs for query comments and articles
    Response comments, articles in forms of json when client send a json via a post.

--- data_api/views.py
+++ data_api/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser
 
 from .models import User, Article, Comment
 from .serializers import UserSerializer, ArticleSerializer, CommentSerializer
@@ -16,3 +19,24 @@ class ArticleViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
+
+@api_view(['POST'])
+def get_comments(request):
+    user_data = JSONParser().parse(request)
+    comments = Comment.objects.filter(poster_id=user_data['id'])
+    comment_serializer = CommentSerializer(comments, many=True)
+    return JsonResponse(comment_serializer.data, safe=False)
+
+@api_view(['POST'])
+def get_article(request):
+    article_data = JSONParser().parse(request)
+    article = Article.objects.get(id=article_data['id'])
+    article_serializer = ArticleSerializer(article)
+    return JsonResponse(article_serializer.data, safe=False)
+
+@api_view(['POST'])
+def get_articles(request):
+    user_data = JSONParser().parse(request)
+    articles = Article.objects.filter(original_poster_id=user_data['id'])
+    article_serializer = ArticleSerializer(articles, many=True)
+    return JsonResponse(article_serializer.data, safe=False)
